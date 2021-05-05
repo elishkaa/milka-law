fa33d3184c69642758fb1d749efcc1b8230e60c7
elishkaa <eliran.avihen@gmail.com>
2021-05-06 00:32:14 +0300

    fix contact me

--- assets/mail/contact_me.js
+++ assets/mail/contact_me.js
@@ -21,7 +21,7 @@ $(function () {
             $this = $("#sendMessageButton");
             $this.prop("disabled", true); // Disable submit button until AJAX call is complete to prevent duplicate messages
             $.ajax({
-                url: "https://script.google.com/macros/s/AKfycbyPwajMrrPuNAiDBWQoxQnTNBkLgiVxU7-RQT_ZlQ/exec",
+                url: "https://script.google.com/macros/s/AKfycbxjMfCo-mHRIMweLoJthWGDfLiIbn09GyNeOrDx1wA0LVbWiGry/exec",
                 type: "POST",
                 data: {
                     name: name,
